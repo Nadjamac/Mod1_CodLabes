389b14bc173f1c8a2e28e8f82081a4a093db4b71
Nadjamac <81447268+Nadjamac@users.noreply.github.com>
2021-05-14 18:18:51 -0300

    Add files via upload

---
+++ Aula8 ex1.py
@@ -0,0 +1,34 @@
+# exercicio aula 8 mod2
+
+#1.	Dada a lista L = [5, 7, 2, 9, 4, 1, 3], escreva um programa que imprima as seguintes informações:
+
+L = [5, 7, 2, 9, 4, 1, 3]
+soma = sum(L)
+L.sort()
+L.sort(reverse=True)
+questaoA= float(input("Qual o tamanho da lista:" ))
+print('O tamanho da lista é de', len(l), 'números!')
+
+questaoB=float(input("Qual o maior numero da lista" ))
+print('O maior número da lista é:', max(l)) 
+
+questaoC=float(input("Qual menor valor da lista:" ))
+print('O menor número da lista é:', min(l))
+
+questaoD=float(input("Qual o valor da soma da lista:"))
+print('A soma de toda lista resulta em:', soma)
+
+
+
+    
+        
+
+
+
+
+
+
+     
+
+
+
