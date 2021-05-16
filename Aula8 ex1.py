9a42aa8fe2bbd8bef720acadde9644eb72150105
Nadjamac <81447268+Nadjamac@users.noreply.github.com>
2021-05-16 15:05:28 -0300

    Update Aula8 ex1.py

--- Aula8 ex1.py
+++ Aula8 ex1.py
@@ -1,34 +1,57 @@
 # exercicio aula 8 mod2
 
-#1.	Dada a lista L = [5, 7, 2, 9, 4, 1, 3], escreva um programa que imprima as seguintes informações:
+# 1.	Dada a lista L = [5, 7, 2, 9, 4, 1, 3], escreva um programa que imprima as seguintes informações:
 
 L = [5, 7, 2, 9, 4, 1, 3]
-soma = sum(L)
-L.sort()
-L.sort(reverse=True)
-questaoA= float(input("Qual o tamanho da lista:" ))
-print('O tamanho da lista é de', len(l), 'números!')
+a = (len(L))
+b = max(L)
+c = min(L)
+d = sum(L)
+exerF = L.sort(reverse=True)
+exerG = L.sort()
 
-questaoB=float(input("Qual o maior numero da lista" ))
-print('O maior número da lista é:', max(l)) 
+questaoA = int(input("Qual a tamanho da lista ? : \n "))
+if  questaoA != a:
+    print(" VOCE ESTA ERRADO !")
+else:
+    print (" PARABENS VOCE ACERTO !")    
 
-questaoC=float(input("Qual menor valor da lista:" ))
-print('O menor número da lista é:', min(l))
+questaoB = int(input("Qual o maior numero da lista: \n "))
 
-questaoD=float(input("Qual o valor da soma da lista:"))
-print('A soma de toda lista resulta em:', soma)
+if questaoB != b:
+    print(" VOCE ESTA ERRADO! ")
+else:
+    print ("PARABENS VOCE ACERTOU! ")    
+
+questaoC = int(input("Qual menor valor da lista: \n "))
+if  questaoC != c :
+
+    print( "VOCE ESTA ERRADO ! ")
+else:    
+    print('PARABENS VOCE ESTA CORRETO !')
+
+questaoD = int(input("Qual o valor da soma da lista: \n "))
+
+if questaoD != d :
+
+    print(" VOCE ESTA ERRADO !")
+else:    
+    print("PARABENS VOCE ESTA CORRETO !")
+
+questaoF = input("Ordene a lista de forma Decrescente: \n ")    
+if questaoF != exerG:
+    print(" ORDEM INCORRETA ! ")
+else :    
+    print(" PARABENS VOCE ESTA CORRETO!")
+
+questaoG = input(" Ordene a lista de forma Crescente ! ")  
+if questaoG != exerF:
+    print (" VOCE ESTA ERRADO !")
+else :
+    print ("PARABENS VOCE ESTA CORRETO !")    
 
 
 
-    
-        
-
-
-
-
-
-
-     
 
 
 
